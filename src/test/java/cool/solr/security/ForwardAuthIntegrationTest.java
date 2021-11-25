cf39cdd90dffdab4868751e2b0568c3c1b6b79f7
Torsten Bøgh Köster <tbk@thiswayup.de>
2021-11-25 12:18:39 +0100

    Tests are working

--- src/test/java/cool/solr/security/ForwardAuthIntegrationTest.java
+++ src/test/java/cool/solr/security/ForwardAuthIntegrationTest.java
@@ -1,6 +1,14 @@
 package cool.solr.security;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -17,17 +25,55 @@ public class ForwardAuthIntegrationTest {
 
     @ClassRule
     public static DockerComposeContainer environment = new DockerComposeContainer(
-            new File("src/test/resources/docker-compose.yaml")).withLocalCompose(true);
+            new File("src/test/resources/docker-compose.yaml"))
+                .withLocalCompose(true);
 
     @BeforeClass
     public static void configureSecurity() throws Exception {
         ContainerState solr = (ContainerState) environment.getContainerByServiceName("solr_1").get();
 
+        synchronized(solr) {
+            solr.wait(2000);
+        }
+
+        // enable the loaded security.json
         solr.execInContainer(SOLR_ENABLE_SECURITY_CMD);
     }
 
     @Test
-    public void solrIsSecured() {
+    public void deniesUnauthenticatedRequests() throws Exception {
+        // given
+        String solrUri = String.format("http://%s:%s", environment.getServiceHost("traefik_1", SOLR_PORT), SOLR_PORT);
+        HttpClient client = HttpClient.newHttpClient();
 
+        // when
+        HttpRequest request = HttpRequest.newBuilder().uri(new URI(solrUri)).GET().build();
+
+        // then
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(401, response.statusCode());
+    }
+
+    @Test
+    public void allowsAuthenticatedRequests() throws Exception {
+        // given
+        String solrUri = String.format("http://%s:%s", environment.getServiceHost("traefik_1", SOLR_PORT), SOLR_PORT);
+        HttpClient client = HttpClient.newBuilder()
+            .authenticator(new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication("solr", "solr".toCharArray());
+                }
+            })
+            .build();
+
+        // when
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI(solrUri))
+            .GET().build();
+
+        // then
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(302, response.statusCode());
     }
 }
