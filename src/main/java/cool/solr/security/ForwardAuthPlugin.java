870c387293a14fe3e70c166e52decbc38ec0948f
Torsten Bøgh Köster <tbk@thiswayup.de>
2021-12-07 11:05:53 +0100

    enable unauthenticated requests

--- src/main/java/cool/solr/security/ForwardAuthPlugin.java
+++ src/main/java/cool/solr/security/ForwardAuthPlugin.java
@@ -16,22 +16,38 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.http.annotation.Contract;
 import org.apache.http.annotation.ThreadingBehavior;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrException.ErrorCode;
 import org.apache.solr.security.AuthenticationPlugin;
+import org.apache.solr.security.BasicAuthPlugin;
 
 public class ForwardAuthPlugin extends AuthenticationPlugin {
 
     public static final String ARG_USER_HEADER = "httpUserHeader";
     public static final String HTTP_HEADER_USER_DEFAULT = "X-Forwarded-User";
+    public static final String X_REQUESTED_WITH_HEADER = "X-Requested-With";
 
     private String httpUserHeader;
+    private boolean blockUnknown = false;
 
     @Override
     public void init(Map<String, Object> args) {
         this.httpUserHeader = (String) args.getOrDefault(ARG_USER_HEADER, HTTP_HEADER_USER_DEFAULT);
+
+        Object o = args.get(BasicAuthPlugin.PROPERTY_BLOCK_UNKNOWN);
+        if (o != null) {
+            try {
+                blockUnknown = Boolean.parseBoolean(o.toString());
+            } catch (Exception e) {
+                throw new SolrException(ErrorCode.BAD_REQUEST,
+                        "Invalid value for parameter " + BasicAuthPlugin.PROPERTY_BLOCK_UNKNOWN);
+            }
+        }
     }
 
     @Override
-    public boolean doAuthenticate(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+    public boolean doAuthenticate(ServletRequest servletRequest, ServletResponse servletResponse,
+            FilterChain filterChain)
             throws Exception {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
@@ -47,9 +63,14 @@ public class ForwardAuthPlugin extends AuthenticationPlugin {
             numAuthenticated.inc();
             filterChain.doFilter(wrapper, response);
             return true;
+        } else if (blockUnknown) {
+            numMissingCredentials.inc();
+            return false;
+        } else {
+            numPassThrough.inc();
+            filterChain.doFilter(request, response);
+            return true;
         }
-
-        return false;
     }
 
     @Contract(threading = ThreadingBehavior.IMMUTABLE)
@@ -76,7 +97,7 @@ public class ForwardAuthPlugin extends AuthenticationPlugin {
                 return true;
             if (o == null || getClass() != o.getClass())
                 return false;
-                ForwardAuthUserPrincipal that = (ForwardAuthUserPrincipal) o;
+            ForwardAuthUserPrincipal that = (ForwardAuthUserPrincipal) o;
             return Objects.equals(username, that.username);
         }
 
